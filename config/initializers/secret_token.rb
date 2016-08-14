019013f5a661ad1a398ed9a959ade05308c928fa
vagrant <vagrant@precise32.(none)>
2016-08-14 01:00:23 +0000

    Initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geojob::Application.config.secret_key_base = '07b7e4d9e99097dc9117a1568173467113c6418f41750de466b71887e2b7854a417fbe858525ecf0a9cb6105a1e70d1b179a97469162242da55951414be5f6cf'
