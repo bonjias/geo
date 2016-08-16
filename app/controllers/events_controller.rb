3263a0bf57411aca38c3022945beeaa9f9b91224
vagrant <vagrant@precise32.(none)>
2016-08-16 02:19:20 +0000

    add events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,22 @@
 class EventsController < ApplicationController
 
   def index
-    @event = Event.first
+    @event = Event.last
 end
+
+
+def new
+  @event = Event.new
+  end
+
+  def create
+    Event.create(event_params)
+    redirect_to root_path
+  end
+
+def event_params
+    params.require(:event).permit(:name, :address)
+  end
+
 
 end
